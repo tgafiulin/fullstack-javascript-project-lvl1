8d399587a6fe7c3c68ac66090c07952c30817d59
Timur Gafiulin <t.gafiulin@ya.ru>
2022-06-27 00:03:19 +0300

    refactoring even

--- src/games/even.js
+++ src/games/even.js
@@ -1,22 +1,15 @@
 import readlineSync from 'readline-sync';
 import { buildAnswerForMistake } from '../utils/utils.js';
 
-const isAnswerCorrect = (number, answer) => {
-  if ((answer === 'yes' && number % 2 === 0) || (answer === 'no' && number % 2 !== 0)) {
-    return true;
-  }
-
-  return false;
-};
-
 const even = () => {
   const randomNumber = Math.round(Math.random() * 100);
+  const realAnswer = randomNumber % 2 === 0 ? 'yes' : 'no';
   console.log(`Question: ${randomNumber}`);
-  const answer = readlineSync.question('Your answer: ');
-  if (isAnswerCorrect(randomNumber, answer)) {
+  const userAnswer = readlineSync.question('Your answer: ');
+  if (realAnswer === answer) {
     console.log('Correct!');
   } else {
-    buildAnswerForMistake(answer, answer === 'yes' ? 'no' : 'yes');
+    buildAnswerForMistake(realAnswer, userAnswer);
     return false;
   }
 
